6870faa49a0fe27820ed60046c0a6ba3a8aa66ad
yl2049 <yl128@illinois.edu>
2023-07-22 14:55:45 -0500

    update HoH 81-99

---
+++ src/modules/data/HoH/enemy.js
@@ -0,0 +1,141 @@
+export default {
+
+
+    // HoH floors 81-89
+    7365: {
+        Name: "天之送狼",
+        Threat: "小心",
+        Aggro: "范围",
+        Weakness: {
+            Sleep: true,
+            Slow: true,
+        },
+        Tip: "寒冰远离，雷电靠近，可打断",
+    },
+    /*
+            { 7375, new MobData { Threat=MobData.ThreatLevel.Dangerous, Aggro=MobData.AggroType.Sight, Vuln = new MobData.Vulnerabilities{CanStun = false}, MobNotes="Cone AoE, circle AoE, partywide damage" } },
+    */
+    7366: {
+        Name: "天之老猿",
+        Threat: "小心",
+        Aggro: "视觉",
+        Weakness: {
+            Sleep: true,
+            Slow: true,
+        },
+        Tip: "脱战状态吃香蕉会提升自身伤害，会对视野内玩家释放AOE并叠加易伤",
+   },
+    7367: {
+        Name: "天之象魔",
+        Threat: "简单",
+        Aggro: "视觉",
+        Weakness: {
+            Sleep: true,
+            Slow: true,
+        },
+        Tip: "开怪时冲锋",
+   },
+    7368: {
+        Name: "天之牛头",
+        Threat: "小心",
+        Aggro: "视觉",
+        Weakness: {
+            Sleep: true,
+            Slow: true,
+        },
+        Tip: "面向随机玩家释放大范围扇形AOE，邪眼攻击",
+    },
+    7369: {
+        Name: "天之龙骨",
+        Threat: "简单",
+        Aggro: "视觉",
+        Weakness: {
+            Sleep: true,
+            Slow: true,
+        },
+        Tip: "圆形AOE，脱战时也会使用",
+    },
+    7370: {
+        Name: "天之龙人",
+        Threat: "小心",
+        Aggro: "视觉",
+        Weakness: {
+            Sleep: true,
+            Slow: true,
+            Stun: true,
+        },
+        Tip: "顺劈，后方有玩家时释放扇形AOE；单人时可引导落肘减少平A",
+    },
+    7371: {
+        Name: "天之独眼",
+        Threat: "小心",
+        Aggro: "视觉",
+        Weakness: {
+            Sleep: true,
+            Slow: true,
+        },
+        Tip: "无范围提示直线AOE和圆形AOE",
+    },
+    7372: {
+        Name: "天之猿猴",
+        Threat: "危险",
+        Aggro: "视觉",
+        Weakness: {
+            Sleep: true,
+            Slow: true,
+            Stun: true,
+        },
+        Tip: "顺劈，开怪一分钟后给自己附加伤害上升BUFF",
+    },
+    7373: {
+        Name: "天之紫衫",
+        Threat: "简单",
+        Aggro: "视觉",
+        Weakness: {
+            Sleep: true,
+            Slow: true,
+        },
+        Tip: "吸引玩家后释放前方扇形AOE，对周围玩家附加DOT，可卡墙躲避",
+    },
+    7374: {
+        Name: "天之钢鬼",
+        Threat: "简单",
+        Aggro: "范围",
+        Weakness: {
+            Sleep: true,
+            Slow: true,
+        },
+        Tip: "大范围扇形AOE，月环，会对周围玩家附加受伤加重，可卡墙躲避",
+    },
+    7375: {
+        Name: "天之加鲁拉",
+        Threat: "危险",
+        Aggro: "视觉",
+        Weakness: {
+            Slow: true,
+        },
+        Tip: "先使用超大扇形aoe，再使用圆形aoe，最后连续吟唱大伤害aoe，可卡墙",
+    },
+    7376: {
+        Name: "天之真济",
+        Threat: "危险",
+        Aggro: "视觉",
+        Weakness: {
+            Sleep: true,
+            Slow: true,
+        },
+        Tip: "双重攻击造成大量伤害并附加dot。震雷会对一个单位点名四次，一次8k。",
+    },
+    7377: {
+        Name: "天之罗刹",
+        Threat: "危险",
+        Aggro: "视觉",
+        Weakness: {
+            Sleep: true,
+            Slow: true,
+        },
+        Tip: "开怪时冲锋，附加伤害上升BUFF，无范围提示前方扇形AOE",
+    },
+
+
+}
