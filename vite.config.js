9f54320283e5aeba83bedf3074a24b7714af85a7
Etest2023 <etest177@126.com>
2023-07-22 13:22:47 +0800

    feat: 支持离线包构建

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,28 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/ff14-overlay-dungeon-cn/",
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const config = {
+    base: "/ff14-overlay-dungeon-cn/",
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
+
+  if(mode === 'offline'){
+    config.base = './'
+    config.plugins.push(legacy({
+      targets: ['defaults', 'not IE 11'],
+    }))
+  }
+
+  return config
 })
